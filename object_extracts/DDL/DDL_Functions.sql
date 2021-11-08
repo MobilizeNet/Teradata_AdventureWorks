0a0b8f8dd40fadeb73370bf0a6e3ab10d01c5e50
MarcoCarrillo-mb <84784241+MarcoCarrillo-mb@users.noreply.github.com>
2021-11-08 15:58:12 -0600

    Update DDL_Functions.sql

--- object_extracts/DDL/DDL_Functions.sql
+++ object_extracts/DDL/DDL_Functions.sql
@@ -1,6 +1,6 @@
 --
 
-/* <sc-function> AdventureWorksDW.age_udf <sc-function> */
+/* <sc-function> AdventureWorksDW.age_udf </sc-function> */
 
 CREATE FUNCTION AdventureWorksDW.age_udf (bith_date DATE)
 RETURNS VARCHAR(100)
@@ -14,7 +14,7 @@ RETURN EXTRACT (YEAR FROM CURRENT_DATE) - EXTRACT (YEAR FROM bith_date );
 
 --
 
-/* <sc-function> AdventureWorksDW.fullname_udf <sc-function> */
+/* <sc-function> AdventureWorksDW.fullname_udf </sc-function> */
 
 CREATE FUNCTION AdventureWorksDW.fullname_udf (account_key INT, account_desc VARCHAR(50))
 RETURNS VARCHAR(100)
